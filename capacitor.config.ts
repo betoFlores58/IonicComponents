33f29fb7100f68b223456349c7a44dc7f7a6ec01
VILLA308 <alberto.x.villareal.flores.-nd@disney.com>
2022-09-26 18:03:00 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'components',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
